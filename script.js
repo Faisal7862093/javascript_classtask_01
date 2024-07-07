4e1fa8843b5ea5746012c629a00669bf968ec0d6
Faisal7862093 <muhammadfaisal7862093@gmail.com>
2024-07-07 11:23:48 +0500

    javascript_classTask_01

---
+++ script.js
@@ -0,0 +1,60 @@
+// Question 01
+
+// var number = +prompt("enter number for table")
+// for(var i=1; i <=10; i++)
+//     {
+//      console.log(`${number}*${i} = ${number * i}`)
+//     }
+//......................................................................
+//Question 02
+
+// var number = [];
+// for(var i=0; i<5; i++)
+//     {
+//         number[i] =+prompt(`enter ${i+1} number`)
+//     }
+// console.log(number)
+// var sum = 0;
+// for(var i=0; i<=4;i++)
+//     {
+//         sum = sum + number[i];
+//     }
+// console.log(`The sum of the number is = ${sum}`)
+
+//.......................................................................
+
+//Question 03
+
+//  var user_number = [];
+// for(var i=0; i<5; i++)
+//   {
+//         user_number[i] =+ prompt("Enter number")
+//     }
+// console.log(user_number)
+// var greater_number = user_number[0];
+// for (var i=0; i<=4 ;i++)
+//     {
+//         if(user_number[i]> greater_number)
+//             {
+//                 greater_number = user_number[i]
+//             }   
+//     }
+// console.log(`The greater number of a above number is = ${greater_number}`)
+
+//................................................................................
+
+// Question 05
+
+// var input = +prompt("Enter number ")
+// var factorial = 1 ;
+// for(var i=1; i <=input; i++)
+//     {
+//        factorial = factorial*i;
+//     }
+//     console.log(`the factorial is = ${factorial}`)
+
+
+
+
+
+
